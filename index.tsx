3dce608d379bcb84e521ca11f646e70271acfc88
ChenB-Y <chenhaziza5941@gmail.com>
2024-07-25 14:13:38 +0300

    c4

--- index.tsx
+++ index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './src/App';
+import 'bootstrap/dist/css/bootstrap.css';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <GoogleOAuthProvider clientId="764938356434-9sg7heli6dv8nj6rlobhhjlq18ui5dpr.apps.googleusercontent.com">
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </GoogleOAuthProvider>
 );
